6c4378256a2e09f9d416a7376e5e8760da81d824
Fernando Ventura <ferventurart@gmail.com>
2023-05-01 13:02:10 -0400

    Cambio visual y estable de la ui

--- AeroPackage.WebApp/Extensions/PackageStatusExtension.cs
+++ AeroPackage.WebApp/Extensions/PackageStatusExtension.cs
@@ -9,16 +9,16 @@ public static class PackageStatusExtension
     {
         return new List<CommonPackageStatus>()
         {
-            new CommonPackageStatus() { Value = "PreAlert", Name = "Registrado", Icon = "img/pre-register.png" },
-            new CommonPackageStatus() { Value = "ReceivedInUsa", Name = "Recibido en USA", Icon = "img/usa.png" },
-            new CommonPackageStatus() { Value = "PreparingForShipment", Name = "Preparando para Env\u00edo", Icon = "img/weight-scale.png" },
-            new CommonPackageStatus() { Value = "InTransitToDestinationCountry", Name = "En Transito a Pa\u00eds Destino", Icon = "img/flight-box.png" },
-            new CommonPackageStatus() { Value = "ReceivedAtDestinationCountry", Name = "Recibido en Pa\u00eds Destino", Icon = "img/el-salvador.png" },
-            new CommonPackageStatus() { Value = "CustomsClearanceInProcess", Name = "Procesandose en Aduana", Icon = "img/custom.png" },
-            new CommonPackageStatus() { Value = "ReleasedFromCustoms", Name = "Liberado en Aduanas", Icon = "img/custom-clear.png" },
-            new CommonPackageStatus() { Value = "StoredInWarehouse", Name = "Almacenado en Bodega ESA", Icon = "img/warehouse.png" },
-            new CommonPackageStatus() { Value = "DeliveredToLocalCourier", Name = "Entregado a Transportista Local", Icon = "img/delivery-in-progress.png" },
-            new CommonPackageStatus() { Value = "Delivered", Name = "Entregado", Icon = "img/delivered.png" },
+            new CommonPackageStatus() { Value = "PreAlert", Name = "Registrado", Icon = "img/package-registered.svg" },
+            new CommonPackageStatus() { Value = "ReceivedInUsa", Name = "Recibido en USA", Icon = "img/package-in-location.svg" },
+            new CommonPackageStatus() { Value = "PreparingForShipment", Name = "Preparando para Env\u00edo", Icon = "img/packages-weigh-scale.svg" },
+            new CommonPackageStatus() { Value = "InTransitToDestinationCountry", Name = "En Transito a Pa\u00eds Destino", Icon = "img/airplane.svg" },
+            new CommonPackageStatus() { Value = "ReceivedAtDestinationCountry", Name = "Recibido en Pa\u00eds Destino", Icon = "img/distribution.svg" },
+            new CommonPackageStatus() { Value = "CustomsClearanceInProcess", Name = "Procesandose en Aduana", Icon = "img/custom-clearance.svg" },
+            new CommonPackageStatus() { Value = "ReleasedFromCustoms", Name = "Liberado en Aduanas", Icon = "img/custom-clearance-free.svg" },
+            new CommonPackageStatus() { Value = "StoredInWarehouse", Name = "Almacenado en Bodega ESA", Icon = "img/warehouse.svg" },
+            new CommonPackageStatus() { Value = "DeliveredToLocalCourier", Name = "Entregado a Transportista Local", Icon = "img/delivery-bike.svg" },
+            new CommonPackageStatus() { Value = "Delivered", Name = "Entregado", Icon = "img/delivery.svg" },
         };
     }
 
